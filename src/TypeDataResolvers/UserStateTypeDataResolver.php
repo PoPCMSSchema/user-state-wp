f4b1368fb6d1af6c1afd03053f1864b59b1d2866
Leo <losoviz@gmail.com>
2020-02-20 19:03:49 +0800

    Fixed filename

--- src/TypeDataResolvers/UserStateTypeDataResolver.php
+++ src/TypeDataResolvers/UserStateTypeDataResolver.php
@@ -7,15 +7,15 @@ class UserStateTypeDataResolver implements UserStateTypeDataResolverInterface {
 
     public function isUserLoggedIn(): bool
     {
-        return is_user_logged_in();
+        return \is_user_logged_in();
     }
     public function getCurrentUser()
     {
-        return wp_get_current_user();
+        return \wp_get_current_user();
     }
     public function getCurrentUserId()
     {
-        return get_current_user_id();
+        return \get_current_user_id();
     }
 
 }
